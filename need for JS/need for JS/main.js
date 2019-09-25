298dd579dfd61e9e3565a07e234daa41529df730
Amiran <49286447+MrPloshka@users.noreply.github.com>
2019-09-25 17:09:04 +0300

    Add files via upload

--- need for JS/need for JS/main.js
+++ need for JS/need for JS/main.js
@@ -29,6 +29,7 @@ const score = document.querySelector('.score'),
 
     function startGame(){
         start.classList.add('hide');
+        gameArea.innerHTML = '';        
         for(let i = 0; i < getQuantityElements(75); i++) {
             const line = document.createElement('div');
             line.classList.add('line');
@@ -46,9 +47,12 @@ const score = document.querySelector('.score'),
             enemy.style.background = 'transparent url(./image/police.png) center / cover no-repeat';
             gameArea.appendChild(enemy);
         }
-
+        setting.score = 0;
         setting.start = true;
         gameArea.appendChild(car);
+        car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+        car.style.top = 'auto';
+        car.style.bottom = '10px';
         setting.x = car.offsetLeft;
         setting.y = car.offsetTop;
         requestAnimationFrame(playGame);
@@ -57,6 +61,8 @@ const score = document.querySelector('.score'),
     function playGame(){
         //console.log('Play Game!');
         if (setting.start === true) {
+            setting.score += setting.speed;
+            score.innerHTML = 'SCORE<br>' + setting.score;
             moveRoad();
             moveEnemy();
             if(keys.ArrowLeft ===true && setting.x >0) {
@@ -105,6 +111,19 @@ const score = document.querySelector('.score'),
     function moveEnemy() {
         let enemy = document.querySelectorAll('.enemy');
         enemy.forEach(function(item){
+            let carRect = car.getBoundingClientRect();
+            let enemyRect = item.getBoundingClientRect();
+
+            if(carRect.top <= enemyRect.bottom &&
+                carRect.right >= enemyRect.left &&
+                carRect.left <= enemyRect.right &&
+                carRect.bottom >= enemyRect.top) {
+                    setting.start = false;
+                    console.warn('ДТП');
+                    start.classList.remove('hide');
+                    start.style.top = score.offsetHeight;
+
+            }
             item.y += setting.speed / 2;
             item.style.top = item.y + 'px';
             if(item.y >= document.documentElement.clientHeight) {
